90226f52a8fdc1bddf2f7e1835f6ba579076439b
Kris De Volder <kdvolder@vmware.com>
2012-11-12 17:36:43 -0800

    Add an 'id' to the protocol. All messages now 'type,topic,id,payload'. Use id to identify connection. Use topic only to identify registered channel on server side.

--- multiplex_client.js
+++ multiplex_client.js
@@ -1,4 +1,12 @@
+/*global setTimeout escape*/
 var WebSocketMultiplex = (function(){
+
+	var newId = (function () {
+		var count = 0;
+		return function () {
+			return count++;
+		};
+	}());
 
 
     // ****
@@ -30,16 +38,17 @@ var WebSocketMultiplex = (function(){
         this.ws = ws;
         this.channels = {};
         this.ws.addEventListener('message', function(e) {
+			//uns and msg should use channel id not channel name to identify their target
             var t = e.data.split(',');
-            var type = t.shift(), name = t.shift(),  payload = t.join();
-            if(!(name in that.channels)) {
+            var type = t.shift(), topic = t.shift(), id = t.shift(),  payload = t.join();
+            if(!(id in that.channels)) {
                 return;
             }
-            var sub = that.channels[name];
+            var sub = that.channels[id];
 
             switch(type) {
             case 'uns':
-                delete that.channels[name];
+                delete that.channels[id];
                 sub.emit('close', {});
                 break;
             case 'msg':
@@ -49,19 +58,21 @@ var WebSocketMultiplex = (function(){
         });
     };
     WebSocketMultiplex.prototype.channel = function(raw_name) {
-        return this.channels[escape(raw_name)] =
-            new Channel(this.ws, escape(raw_name), this.channels);
+		var id = newId();
+		var ch = new Channel(this.ws, escape(raw_name), id, this.channels);
+        this.channels[id] = ch;
+        return ch;
     };
 
-
-    var Channel = function(ws, name, channels) {
+    var Channel = function(ws, name, id, channels) {
         DumbEventTarget.call(this);
         var that = this;
         this.ws = ws;
         this.name = name;
+        this.id = id;
         this.channels = channels;
         var onopen = function() {
-            that.ws.send('sub,' + that.name);
+            that.ws.send('sub,' + that.name+','+that.id);
             that.emit('open');
         };
         if(ws.readyState > 0) {
@@ -70,15 +81,15 @@ var WebSocketMultiplex = (function(){
             this.ws.addEventListener('open', onopen);
         }
     };
-    Channel.prototype = new DumbEventTarget()
+    Channel.prototype = new DumbEventTarget();
 
     Channel.prototype.send = function(data) {
-        this.ws.send('msg,' + this.name + ',' + data);
+        this.ws.send('msg,' + this.name + ',' +this.id + ',' + data);
     };
     Channel.prototype.close = function() {
         var that = this;
-        this.ws.send('uns,' + this.name);
-        delete this.channels[this.name];
+        this.ws.send('uns,' + this.name + ',' + this.id);
+        delete this.channels[this.id];
         setTimeout(function(){that.emit('close', {});},0);
     };
 
